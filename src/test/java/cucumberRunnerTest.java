a2cb37e61483d91611f2ddd9b7bb3556e95e079b
x00180702 <90932427+x00180702@users.noreply.github.com>
2022-09-27 16:49:37 +0100

    Update cucumberRunnerTest.java

--- src/test/java/cucumberRunnerTest.java
+++ src/test/java/cucumberRunnerTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         glue = {"com.FeatureStepDefs"},
         plugin = {"json:target/cucumber.json",
                   "html:target/target/cucumber-html-report.html"},
-        tags = "@bpTest or @bpcTest"
+        tags = "@bpTest or @bpcTest",
         publish = true
 
 
