d9b806faae3b6591bec5bfef36e7b57cf9d0cec8
jgunter <x00180702@mytudublin.ie>
2022-10-05 15:48:10 +0100

    update url

--- src/main/java/helpers/Utils.java
+++ src/main/java/helpers/Utils.java
@@ -2,6 +2,6 @@ package helpers;// Configuration class for Form Automation project
 
 public class Utils {
     final static String BASE_URL = "https://bloodpressurecalculator.azurewebsites.net";
-    final static String BASE_URL1 = "http://20.150.152.147:22137";
+    final static String BASE_URL1 = "http://20.14.20.35:22137";
     final static String CHROME_DRIVER_LOCATION = "chromedriver";
 }
