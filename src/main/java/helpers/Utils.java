e30a0a0f81bd688cb4254bcbc4c5f5cd05142642
x00180702 <90932427+x00180702@users.noreply.github.com>
2022-11-07 16:31:39 +0000

    Update Utils.java

--- src/main/java/helpers/Utils.java
+++ src/main/java/helpers/Utils.java
@@ -2,6 +2,6 @@ package helpers;// Configuration class for Form Automation project
 
 public class Utils {
     final static String BASE_URL = "https://bloodpressurecalculator.azurewebsites.net";
-    final static String BASE_URL1 = "http://20.90.193.20:22137";
+    final static String BASE_URL1 = "http://20.26.164.96:22137";
     final static String CHROME_DRIVER_LOCATION = "chromedriver";
 }
